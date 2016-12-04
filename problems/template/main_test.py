18e659913dbc805bd179b3a142432b3bfccc0652
StaNov <stanovec@gmail.com>
2016-12-04 20:40:54 +0100

    added template day

---
+++ problems/template/main_test.py
@@ -0,0 +1,9 @@
+from .main import Main
+
+
+def test_main_1():
+    assert Main.main_1() == 0
+
+
+def test_main_2():
+    assert Main.main_2() == 0
---
+++ problems/template/solver_test.py
@@ -0,0 +1,9 @@
+from .solver import Solver
+
+
+def test_1_1():
+    assert Solver.solve_1("test test") == 0
+
+
+def test_2_1():
+    assert Solver.solve_2("test test") == 0
