7cd019ed965e08db23da9b1b0f1795920c510e5e
StaNov <stanovec@gmail.com>
2016-12-04 20:42:15 +0100

    added day two

---
+++ problems/day02/main.py
@@ -0,0 +1,25 @@
+from utils import InputReader
+if __name__ == "__main__":
+    from problems.day02.solver import Solver
+else:
+    from .solver import Solver
+
+
+class Main:
+    @staticmethod
+    def main_1():
+        content = InputReader.read_input(__file__)
+        return Solver.solve_1(content)
+
+    @staticmethod
+    def main_2():
+        content = InputReader.read_input(__file__)
+        return Solver.solve_2(content)
+
+
+if __name__ == "__main__":
+    result_1 = Main.main_1()
+    print("FIRST RESULT IS: " + str(result_1) + " !!!")
+
+    result_2 = Main.main_2()
+    print("SECOND RESULT IS: " + str(result_2) + " !!!")
---
+++ problems/day02/solver.py
@@ -0,0 +1,8 @@
+class Solver:
+    @staticmethod
+    def solve_1(input_string):
+        return 0
+
+    @staticmethod
+    def solve_2(input_string):
+        return 0
