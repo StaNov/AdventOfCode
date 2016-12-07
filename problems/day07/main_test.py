fe3ca0dca4eb89a371e35e077735a7df3dcc9038
StaNov <stanovec@gmail.com>
2016-12-07 20:33:29 +0100

    added day07

---
+++ problems/day07/main_test.py
@@ -0,0 +1,11 @@
+from .main import Main
+
+
+def test_main_1():
+    # TODO assert Main.main_1() == 0
+    pass
+
+
+def test_main_2():
+    # TODO assert Main.main_2() == 0
+    pass
---
+++ problems/day07/solver.py
@@ -0,0 +1,11 @@
+# from .internal import Class
+
+
+class Solver:
+    @staticmethod
+    def solve_1(input_string):
+        return 0  # TODO
+
+    @staticmethod
+    def solve_2(input_string):
+        return 0  # TODO
