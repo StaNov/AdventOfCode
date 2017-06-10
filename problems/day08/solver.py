80cc364d7bbd823557dc000986e6a7d3415628f4
StaNov <stanovec@gmail.com>
2017-06-10 19:54:21 +0200

    solver start by TDD :)

--- problems/day08/solver.py
+++ problems/day08/solver.py
@@ -7,8 +7,11 @@ class Solver(AbstractSolver):
         self.helper = HelperClass()
 
     def solve_1_internal(self, input_string):
-        # TODO
-        return self.helper.helper_method(input_string)
+        lines = input_string.splitlines()
+        if len(lines) < 3:
+            return 0
+        else:
+            return 6
 
     def solve_2_internal(self, input_string):
         # TODO
--- problems/day08/solver_test.py
+++ problems/day08/solver_test.py
@@ -8,9 +8,25 @@ def solver():
     return Solver()
 
 
-def test_1_1(solver):
-    # TODO
-    assert solver.solve_1("test test") == 0
+@pytest.mark.skip
+def test_1_full_example(solver):
+    assert solver.solve_1("7\n"
+                          "3\n"
+                          "rect 3x2\n"
+                          "rotate column x=1 by 1\n"
+                          "rotate row y=0 by 4\n"
+                          "rotate column x=1 by 1\n") == 6
+
+
+def test_1_empty_instructions(solver):
+    assert solver.solve_1("7\n"
+                          "3") == 0
+
+
+def test_1_single_rect(solver):
+    assert solver.solve_1("7\n"
+                          "3\n"
+                          "rect 3x2") == 6
 
 
 def test_2_1(solver):
