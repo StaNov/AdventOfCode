3749466001c43445b589913098bfcb4cfd5e611f
StaNov <stanovec@gmail.com>
2017-06-21 20:22:48 +0200

    first TDD test

--- problems/day09/solver_test.py
+++ problems/day09/solver_test.py
@@ -8,9 +8,20 @@ def solver():
     return Solver()
 
 
+# TODO
+@pytest.mark.skip
+def test_1_full_example(solver):
+    assert solver.solve_1("ADVENT") == 6
+    assert solver.solve_1("A(1x5)BC") == 7
+    assert solver.solve_1("(3x3)XYZ") == 9
+    assert solver.solve_1("A(2x2)BCD(2x2)EFG") == 11
+    assert solver.solve_1("(6x1)(1x3)A") == 6
+    assert solver.solve_1("X(8x2)(3x3)ABCY") == 18
+
+
 def test_1_1(solver):
     # TODO
-    assert solver.solve_1("test test") == 0
+    assert solver.solve_1("") == 0
 
 
 def test_2_1(solver):
