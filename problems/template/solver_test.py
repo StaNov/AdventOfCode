a811d55e6c5cb8a5711438d19c6c6cca3d3bf1c8
StaNov <stanovec@gmail.com>
2017-06-21 20:19:55 +0200

    full example tests in template

--- problems/template/solver_test.py
+++ problems/template/solver_test.py
@@ -8,11 +8,21 @@ def solver():
     return Solver()
 
 
+def test_1_full_example(solver):
+    # TODO
+    assert solver.solve_1("test test") == 0
+
+
 def test_1_1(solver):
     # TODO
     assert solver.solve_1("test test") == 0
 
 
 def test_2_1(solver):
+    # TODO
+    assert solver.solve_2("test test") == 0
+
+
+def test_2_full_example(solver):
     # TODO
     assert solver.solve_2("test test") == 0
