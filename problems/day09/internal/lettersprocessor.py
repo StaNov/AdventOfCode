46ee23f89be323a7f5f38cba0ac15be12858ecfc
StaNov <stanovec@gmail.com>
2017-06-22 19:50:59 +0200

    new state pattern, just read redone to that

--- problems/day09/internal/lettersprocessor.py
+++ problems/day09/internal/lettersprocessor.py
@@ -6,7 +6,8 @@ import re
 class LettersProcessor:
     def __init__(self):
         self.output = ""
-        self.state = LettersProcessorState.JUST_READING
+        self.stateOld = LettersProcessorStateOld.JUST_READING
+        self.state = StateJustReading()
         self.parentheses_content = ""
         self.what_to_repeat = ""
         self.what_to_repeat_length = 0
@@ -16,25 +17,17 @@ class LettersProcessor:
         if len(letter) != 1:
             raise Exception("Argument must be one letter!")
 
-        if self.state == LettersProcessorState.JUST_READING:
-            self._just_read(letter)
+        if self.stateOld == LettersProcessorStateOld.JUST_READING:
+            self.state.process_letter(self, letter)
             return
 
-        if self.state == LettersProcessorState.READING_PARENTHESES_CONTAINS:
+        if self.stateOld == LettersProcessorStateOld.READING_PARENTHESES_CONTAINS:
             self._read_parentheses_content(letter)
             return
 
-        if self.state == LettersProcessorState.READING_WHAT_TO_REPEAT:
+        if self.stateOld == LettersProcessorStateOld.READING_WHAT_TO_REPEAT:
             self._read_what_to_repeat(letter)
             return
-
-    def _just_read(self, letter):
-        if letter != "(":
-            self.output = self.output + letter
-            return
-
-        self.parentheses_content = ""
-        self.state = LettersProcessorState.READING_PARENTHESES_CONTAINS
 
     def _read_parentheses_content(self, letter):
         if letter == "(":
@@ -53,7 +46,7 @@ class LettersProcessor:
         self.what_to_repeat_how_many = int(match.group(2))
         self.what_to_repeat = ""
 
-        self.state = LettersProcessorState.READING_WHAT_TO_REPEAT
+        self.stateOld = LettersProcessorStateOld.READING_WHAT_TO_REPEAT
 
     def _read_what_to_repeat(self, letter):
         self.what_to_repeat = self.what_to_repeat + letter
@@ -61,7 +54,13 @@ class LettersProcessor:
         if len(self.what_to_repeat) == self.what_to_repeat_length:
             self.output = self.output + self.what_to_repeat_how_many * self.what_to_repeat
             self.parentheses_content = ""
-            self.state = LettersProcessorState.JUST_READING
+            self.stateOld = LettersProcessorStateOld.JUST_READING
+
+    def _set_state(self, state):
+        pass # TODO
+
+    def add_to_output(self, content_to_add):
+        self.output = self.output + content_to_add
 
     def get_processed_output(self):
         if self.parentheses_content != "":
@@ -70,7 +69,25 @@ class LettersProcessor:
         return self.output
 
 
-class LettersProcessorState(Enum):
+# TODO delete
+class LettersProcessorStateOld(Enum):
     JUST_READING = 1
     READING_PARENTHESES_CONTAINS = 2
     READING_WHAT_TO_REPEAT = 3
+
+
+class LettersProcessorState:
+    def process_letter(self, caller, letter):
+        raise Exception("Not implemented!")
+
+
+class StateJustReading(LettersProcessorState):
+    def process_letter(self, caller, letter):
+        if letter != "(":
+            caller.add_to_output(letter)
+            return
+
+        caller.parentheses_content = ""
+        # TODO swap
+        caller.stateOld = LettersProcessorStateOld.READING_PARENTHESES_CONTAINS
+        #caller._set_state(StateReadingParenthesesContent)
