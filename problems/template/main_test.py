d14f49b5e166ab42841f5188639930a9752f70fd
StaNov <stanovec@gmail.com>
2017-06-22 23:35:06 +0200

    comment about abstract main tests

--- problems/template/main_test.py
+++ problems/template/main_test.py
@@ -7,6 +7,10 @@ def main():
     return MainCalculator()
 
 
+# TODO do it in abstract way?
+# def test_main():
+#     main.test_both_parts("expected_result_1", "expected_result_2")
+# make argument None if the part should not be tested
 def test_main_1(main):
     # TODO
     assert main.calculate_part_1() == 0
