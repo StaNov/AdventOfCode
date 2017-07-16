1b3969006c990d757df74120ffa86e9b9445a0bf
StaNov <stanovec@gmail.com>
2017-07-16 21:51:05 +0200

    added days folder integrity test

---
+++ problems/days_folder_integrity_test.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+
+def test_directory_names_in_days():
+    days_folder_path = os.path.join(os.path.dirname(__file__), "days")
+
+    for folder_name in os.listdir(days_folder_path):
+        _check_folder_name(folder_name)
+
+
+def _check_folder_name(folder_name):
+    match = re.match("day\d\d|template|__pycache__", folder_name)
+
+    if not match:
+        raise Exception("Folder '" + folder_name + "' does not belong to the days folder!")
