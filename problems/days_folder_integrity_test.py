f553dcaa73d5b95ba41c18d031a4a80a5b46df7b
StaNov <stanovec@gmail.com>
2017-07-28 21:08:58 +0200

    added .cache folder to valid folders in days

--- problems/days_folder_integrity_test.py
+++ problems/days_folder_integrity_test.py
@@ -1,6 +1,9 @@
 import os
 import re
 
+VALID_FOLDER_NAMES = ["day\d\d", "template", "__pycache__", ".cache"]
+VALID_FOLDER_NAMES_REGEX = str.join("|", VALID_FOLDER_NAMES)
+
 
 def test_directory_names_in_days():
     days_folder_path = os.path.join(os.path.dirname(__file__), "days")
@@ -10,7 +13,7 @@ def test_directory_names_in_days():
 
 
 def _check_folder_name(folder_name):
-    match = re.fullmatch("day\d\d|template|__pycache__", folder_name)
+    match = re.fullmatch(VALID_FOLDER_NAMES_REGEX, folder_name)
 
     if not match:
         raise Exception("Folder '" + folder_name + "' does not belong to the days folder!")
