5bd1f746e74d44834ae342ce39a57eb3464d43c9
StaNov <stanovec@gmail.com>
2017-12-02 22:45:29 +0100

    result added to main test

--- problems/days/day201/main_test.py
+++ problems/days/day201/main_test.py
@@ -2,19 +2,13 @@ import pytest
 from .main import DayCalculator
 
 
-# TODO rename to day_calculator
 @pytest.fixture
 def main():
     return DayCalculator()
 
 
-# TODO do it in abstract way?
-# def test_main():
-#     main.test_both_parts("expected_result_1", "expected_result_2")
-# make argument None if the part should not be tested
 def test_main_1(main):
-    # TODO
-    assert main.calculate_part_1() == 0
+    assert main.calculate_part_1() == 1158
 
 
 def test_main_2(main):
