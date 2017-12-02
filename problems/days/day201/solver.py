be8b9ebfe046b0d513427e65090cbefb911c3831
StaNov <stanovec@gmail.com>
2017-12-02 22:57:26 +0100

    added params to shared solve function

--- problems/days/day201/solver.py
+++ problems/days/day201/solver.py
@@ -8,22 +8,22 @@ class Solver(AbstractSolver):
         self.helper = HelperClass()
 
     def solve_1_internal(self, input_string):
-        return self.solve(input_string)
+        return self.solve(input_string, len(input_string) - 1, 1, True)
 
     def solve_2_internal(self, input_string):
-        return self.solve(input_string)
+        return self.solve(input_string, len(input_string) / 2, len(input_string) / 2, False)
 
-    def solve(self, input_string):
+    def solve(self, input_string, range_upper_bound, second_digit_index_offset, include_circular):
         result = 0
 
-        for i in range(len(input_string) - 1):
+        for i in range(range_upper_bound):
             digit_1 = input_string[i]
-            digit_2 = input_string[i + 1]
+            digit_2 = input_string[i + second_digit_index_offset]
 
             if digit_1 == digit_2:
                 result += int(digit_1)
 
-        if len(input_string) > 2 and input_string[0] == input_string[-1]:
+        if include_circular and len(input_string) > 2 and input_string[0] == input_string[-1]:
             result += int(input_string[0])
 
         return result
