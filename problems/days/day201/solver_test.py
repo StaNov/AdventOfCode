d3b5104a05192dc61968b4e413ac2538f7823e6d
StaNov <stanovec@gmail.com>
2017-12-02 22:43:40 +0100

    acceptance tests skip removed

--- problems/days/day201/solver_test.py
+++ problems/days/day201/solver_test.py
@@ -8,22 +8,18 @@ def solver():
     return Solver()
 
 
-@pytest.mark.skip
 def test_1_full_example_1(solver):
     assert solver.solve_1("1122") == 3
 
 
-@pytest.mark.skip
 def test_1_full_example_2(solver):
     assert solver.solve_1("1111") == 4
 
 
-@pytest.mark.skip
 def test_1_full_example_3(solver):
     assert solver.solve_1("1234") == 0
 
 
-@pytest.mark.skip
 def test_1_full_example_4(solver):
     assert solver.solve_1("91212129") == 9
 
