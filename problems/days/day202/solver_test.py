68a8de6b9cf31cd796c92d85eb7f555b3b08deec
StaNov <stanovec@gmail.com>
2017-12-04 21:17:27 +0100

    added input and tests from the web page

--- problems/days/day202/solver_test.py
+++ problems/days/day202/solver_test.py
@@ -8,14 +8,29 @@ def solver():
     return Solver()
 
 
-def test_1_full_example(solver):
-    # TODO
-    assert solver.solve_1("test test") == 0
-
-
 def test_1_1(solver):
     # TODO
     assert solver.solve_1("test test") == 0
+
+
+@pytest.mark.skip
+def test_1_full_example_row_1(solver):
+    assert solver.solve_1("5 1 9 5") == 8
+
+
+@pytest.mark.skip
+def test_1_full_example_row_2(solver):
+    assert solver.solve_1("7 5 3") == 4
+
+
+@pytest.mark.skip
+def test_1_full_example_row_3(solver):
+    assert solver.solve_1("2 4 6 8") == 6
+
+
+@pytest.mark.skip
+def test_1_full_example(solver):
+    assert solver.solve_1("5 1 9 5\n7 5 3\n2 4 6 8") == 18
 
 
 def test_2_1(solver):
