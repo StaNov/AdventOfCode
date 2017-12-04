adaef96a15089da4171fd6d45cef6f7ecfbc66ac
StaNov <stanovec@gmail.com>
2017-12-04 20:43:52 +0100

    day 202 created from template

---
+++ problems/days/day202/solver_test.py
@@ -0,0 +1,28 @@
+import pytest
+
+from .solver import Solver
+
+
+@pytest.fixture
+def solver():
+    return Solver()
+
+
+def test_1_full_example(solver):
+    # TODO
+    assert solver.solve_1("test test") == 0
+
+
+def test_1_1(solver):
+    # TODO
+    assert solver.solve_1("test test") == 0
+
+
+def test_2_1(solver):
+    # TODO
+    assert solver.solve_2("test test") == 0
+
+
+def test_2_full_example(solver):
+    # TODO
+    assert solver.solve_2("test test") == 0
