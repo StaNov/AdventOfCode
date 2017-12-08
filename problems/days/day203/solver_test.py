80491e6707eead9c41c99447d321a9efe8937fef
StaNov <stanovec@gmail.com>
2017-12-08 23:46:44 +0100

    part 2 full tests

--- problems/days/day203/solver_test.py
+++ problems/days/day203/solver_test.py
@@ -24,11 +24,21 @@ def test_1_full_example_4(solver):
     assert solver.solve_1("1024") == 31
 
 
-def test_2_1(solver):
-    # TODO
-    assert solver.solve_2("test test") == 0
+@pytest.mark.skip
+def test_2_full_example_1(solver):
+    assert solver.solve_2("0") == 1
 
 
-def test_2_full_example(solver):
-    # TODO
-    assert solver.solve_2("test test") == 0
+@pytest.mark.skip
+def test_2_full_example_2(solver):
+    assert solver.solve_2("1") == 2
+
+
+@pytest.mark.skip
+def test_2_full_example_3(solver):
+    assert solver.solve_2("2") == 4
+
+
+@pytest.mark.skip
+def test_2_full_example_4(solver):
+    assert solver.solve_2("3") == 5
