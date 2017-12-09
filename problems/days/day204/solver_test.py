7239dafdb10e2100544523f5e10203ce3d4ee176
StaNov <stanovec@gmail.com>
2017-12-09 11:16:58 +0100

    part 2 full example

--- problems/days/day204/solver_test.py
+++ problems/days/day204/solver_test.py
@@ -8,29 +8,18 @@ def solver():
     return Solver()
 
 
-def test_1_full_example_1(solver):
-    assert solver.solve_1("aa bb cc dd ee") == 1
+def test_1_full_example(solver):
+    assert 2 == solver.solve_1(
+        "aa bb cc dd ee\n"
+        "aa bb cc dd aa\n"
+        "aa bb cc dd aaa")
 
 
-def test_1_full_example_2(solver):
-    assert solver.solve_1("aa bb cc dd aa") == 0
-
-
-def test_1_full_example_3(solver):
-    assert solver.solve_1("aa bb cc dd aaa") == 1
-
-
-def test_1_full_example_4(solver):
-    assert solver.solve_1("aa bb cc dd ee\naa bb cc dd aa\naa bb cc dd aaa") == 2
-
-
-def test_2_1(solver):
-    # TODO
-    # assert solver.solve_2("test test") == 0
-    pass
-
-
+@pytest.mark.skip
 def test_2_full_example(solver):
-    # TODO
-    # assert solver.solve_2("test test") == 0
-    pass
+    assert 3 == solver.solve_2(
+        "abcde fghij\n"
+        "abcde xyz ecdab\n"
+        "a ab abc abd abf abj\n"
+        "iiii oiii ooii oooi oooo\n"
+        "oiii ioii iioi iiio")
