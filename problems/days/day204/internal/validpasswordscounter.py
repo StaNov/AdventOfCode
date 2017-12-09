c78e364edc46954d54de8be8013e2a4057953cef
StaNov <stanovec@gmail.com>
2017-12-09 12:02:41 +0100

    counter now takes checker as constructor argument

--- problems/days/day204/internal/validpasswordscounter.py
+++ problems/days/day204/internal/validpasswordscounter.py
@@ -1,9 +1,12 @@
 class ValidPasswordsCounter:
-    def count(self, input_string, checker):
+    def __init__(self, checker):
+        self.checker = checker
+
+    def count(self, input_string):
         valid_passwords_count = 0
 
         for line in input_string.splitlines():
-            if checker.check(line):
+            if self.checker.check(line):
                 valid_passwords_count += 1
 
         return valid_passwords_count
--- problems/days/day204/internal/validpasswordscounter_test.py
+++ problems/days/day204/internal/validpasswordscounter_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from . import ValidPasswordsCounter
 
 
@@ -8,21 +10,31 @@ class TestChecker:
         return password == self.VALID_LINE
 
 
-def test_zero_valid_passwords_in_empty_string():
-    assert 0 == ValidPasswordsCounter().count("", TestChecker())
+@pytest.fixture
+def checker():
+    return TestChecker()
 
 
-def test_one_valid_password():
-    assert 1 == ValidPasswordsCounter().count("validPassword", TestChecker())
+@pytest.fixture
+def counter(checker):
+    return ValidPasswordsCounter(checker)
 
 
-def test_one_invalid_password():
-    assert 0 == ValidPasswordsCounter().count("invalid", TestChecker())
+def test_zero_valid_passwords_in_empty_string(counter):
+    assert 0 == counter.count("")
 
 
-def test_two_valid_passwords():
-    assert 2 == ValidPasswordsCounter().count("validPassword\nvalidPassword", TestChecker())
+def test_one_valid_password(counter):
+    assert 1 == counter.count("validPassword")
 
 
-def test_valid_and_invalid_passwords():
-    assert 1 == ValidPasswordsCounter().count("validPassword\ninvalid", TestChecker())
+def test_one_invalid_password(counter):
+    assert 0 == counter.count("invalid")
+
+
+def test_two_valid_passwords(counter):
+    assert 2 == counter.count("validPassword\nvalidPassword")
+
+
+def test_valid_and_invalid_passwords(counter):
+    assert 1 == counter.count("validPassword\ninvalid")
