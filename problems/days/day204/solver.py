98e53bb65b51da645aa27c56f8116ace6b6490e5
StaNov <stanovec@gmail.com>
2017-12-09 11:01:45 +0100

    solver implemented

--- problems/days/day204/solver.py
+++ problems/days/day204/solver.py
@@ -5,12 +5,16 @@ from .internal import PasswordChecker
 class Solver(AbstractSolver):
     def __init__(self):
         super().__init__()
-        self.helper = PasswordChecker()
+        self.checker = PasswordChecker()
 
     def solve_1_internal(self, input_string):
-        # TODO
-        # return self.helper.helper_method(input_string)
-        return 0
+        valid_passwords_count = 0
+
+        for line in input_string.splitlines():
+            if self.checker.check(line):
+                valid_passwords_count += 1
+
+        return valid_passwords_count
 
     def solve_2_internal(self, input_string):
         # TODO
--- problems/days/day204/solver_test.py
+++ problems/days/day204/solver_test.py
@@ -8,22 +8,18 @@ def solver():
     return Solver()
 
 
-@pytest.mark.skip
 def test_1_full_example_1(solver):
     assert solver.solve_1("aa bb cc dd ee") == 1
 
 
-@pytest.mark.skip
 def test_1_full_example_2(solver):
     assert solver.solve_1("aa bb cc dd aa") == 0
 
 
-@pytest.mark.skip
 def test_1_full_example_3(solver):
     assert solver.solve_1("aa bb cc dd aaa") == 1
 
 
-@pytest.mark.skip
 def test_1_full_example_4(solver):
     assert solver.solve_1("aa bb cc dd ee\naa bb cc dd aa\naa bb cc dd aaa") == 2
 
