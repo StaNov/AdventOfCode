22e19984a4148768ac913915af492f4934342f3a
StaNov <stanovec@gmail.com>
2017-12-09 11:55:14 +0100

    valid passwords counter done

--- problems/days/day204/internal/validpasswordscounter.py
+++ problems/days/day204/internal/validpasswordscounter.py
@@ -1,3 +1,9 @@
 class ValidPasswordsCounter:
     def count(self, input_string, checker):
-        return 0
+        valid_passwords_count = 0
+
+        for line in input_string.splitlines():
+            if checker.check(line):
+                valid_passwords_count += 1
+
+        return valid_passwords_count
--- problems/days/day204/internal/validpasswordscounter_test.py
+++ problems/days/day204/internal/validpasswordscounter_test.py
@@ -1,10 +1,8 @@
-import pytest
-
 from . import ValidPasswordsCounter
 
 
 class TestChecker:
-    VALID_LINE = "abc"
+    VALID_LINE = "validPassword"
 
     def check(self, password):
         return password == self.VALID_LINE
@@ -14,6 +12,17 @@ def test_zero_valid_passwords_in_empty_string():
     assert 0 == ValidPasswordsCounter().count("", TestChecker())
 
 
-@pytest.mark.skip
 def test_one_valid_password():
-    assert 1 == ValidPasswordsCounter().count("abc", TestChecker())
+    assert 1 == ValidPasswordsCounter().count("validPassword", TestChecker())
+
+
+def test_one_invalid_password():
+    assert 0 == ValidPasswordsCounter().count("invalid", TestChecker())
+
+
+def test_two_valid_passwords():
+    assert 2 == ValidPasswordsCounter().count("validPassword\nvalidPassword", TestChecker())
+
+
+def test_valid_and_invalid_passwords():
+    assert 1 == ValidPasswordsCounter().count("validPassword\ninvalid", TestChecker())
