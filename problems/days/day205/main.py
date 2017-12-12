650572c1145ca1006e485ddcaba747c184775f06
StaNov <stanovec@gmail.com>
2017-12-12 21:03:59 +0100

    day205 copied from template, input inserted

---
+++ problems/days/day205/main.py
@@ -0,0 +1,22 @@
+from .inputfileparser import InputFileParser
+from problems.utils import AbstractDayCalculator
+
+if __name__ == "__main__":
+    from problems.days.template.solver import Solver
+else:
+    from .solver import Solver
+
+
+class DayCalculator(AbstractDayCalculator):
+    def get_main_calculator_file_path(self):
+        return __file__
+
+    def create_new_solver(self):
+        return Solver()
+
+    def get_input_file_parser(self):
+        return InputFileParser()
+
+
+if __name__ == "__main__":
+    DayCalculator().calculate_both_parts_and_print_results()
