b56af16722b297ce01cab073eb890e4e71b39d12
StaNov <stanovec@gmail.com>
2017-12-12 21:02:48 +0100

    added TODOs to template's file parser

--- problems/days/template/inputfileparser.py
+++ problems/days/template/inputfileparser.py
@@ -3,4 +3,5 @@ from problems.utils import InputFileParser as BaseParser
 
 class InputFileParser(BaseParser):
     def parse(self, input_string):
+        # TODO
         return input_string.splitlines()
--- problems/days/template/inputfileparser_test.py
+++ problems/days/template/inputfileparser_test.py
@@ -2,4 +2,5 @@ from .inputfileparser import InputFileParser
 
 
 def test_parse():
+    # TODO
     assert ["abc", "def", "ghi"] == InputFileParser().parse("abc\ndef\nghi")
