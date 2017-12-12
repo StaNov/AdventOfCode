47938724ab8d2c66a05a350465db9b43d3ff42e9
StaNov <stanovec@gmail.com>
2017-12-12 21:39:57 +0100

    acceptance test for solver part 1

--- problems/days/day205/solver_test.py
+++ problems/days/day205/solver_test.py
@@ -8,19 +8,10 @@ def solver():
     return Solver()
 
 
+# TODO
+@pytest.mark.skip
 def test_1_full_example(solver):
-    # TODO
-    assert solver.solve_1("test test") == 0
-
-
-def test_1_1(solver):
-    # TODO
-    assert solver.solve_1("test test") == 0
-
-
-def test_2_1(solver):
-    # TODO
-    assert solver.solve_2("test test") == 0
+    assert 5 == solver.solve_1([0, 3, 0, 1, -3])
 
 
 def test_2_full_example(solver):
