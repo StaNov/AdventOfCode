8ba56ec46941fb5f58f00c635df94ffad32e1efe
StaNov <stanovec@gmail.com>
2017-12-13 21:36:24 +0100

    solver test for part 2

--- problems/days/day205/solver_test.py
+++ problems/days/day205/solver_test.py
@@ -12,6 +12,6 @@ def test_1_full_example(solver):
     assert 5 == solver.solve_1([0, 3, 0, 1, -3])
 
 
+@pytest.mark.skip
 def test_2_full_example(solver):
-    # TODO
-    assert solver.solve_2("test test") == 0
+    assert 10 == solver.solve_2([2, 3, 2, 3, -1])
