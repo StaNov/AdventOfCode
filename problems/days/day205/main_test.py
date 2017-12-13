b060ef5821189b3fda9ea024d8e7667cfe9a64ab
StaNov <stanovec@gmail.com>
2017-12-13 22:06:29 +0100

    solver test uncommented

--- problems/days/day205/main_test.py
+++ problems/days/day205/main_test.py
@@ -11,6 +11,7 @@ def test_main_1(main):
     assert 375042 == main.calculate_part_1()
 
 
+@pytest.mark.skip
 def test_main_2(main):
     # TODO
     assert main.calculate_part_2() == 0
--- problems/days/day205/solver_test.py
+++ problems/days/day205/solver_test.py
@@ -12,6 +12,5 @@ def test_1_full_example(solver):
     assert 5 == solver.solve_1([0, 3, 0, 1, -3])
 
 
-@pytest.mark.skip
 def test_2_full_example(solver):
     assert 10 == solver.solve_2([0, 3, 0, 1, -3])
