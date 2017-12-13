1f3646230fc932f129f1b6d5d9fee7ba9b17549d
StaNov <stanovec@gmail.com>
2017-12-13 21:25:07 +0100

    do step on finished maze raises exception

--- problems/days/day205/internal/trampolinemaze.py
+++ problems/days/day205/internal/trampolinemaze.py
@@ -3,6 +3,7 @@ class TrampolineMaze:
     def __init__(self, values):
         self._values = values
         self._steps_done = 0
+        self._current_index = 0
 
     def process_until_finished(self):
         while not self.is_finished():
@@ -12,7 +13,14 @@ class TrampolineMaze:
         return len(self._values) == 0 or len(self._values) == self._steps_done
 
     def do_step(self):
+        if self.is_finished():
+            raise CannotDoStepOnFinishedMaze()
+
         self._steps_done += 1
 
     def get_steps_done(self):
         return self._steps_done
+
+
+class CannotDoStepOnFinishedMaze(Exception):
+    pass
--- problems/days/day205/internal/trampolinemaze_test.py
+++ problems/days/day205/internal/trampolinemaze_test.py
@@ -1,4 +1,6 @@
-from .trampolinemaze import TrampolineMaze
+import pytest
+
+from .trampolinemaze import TrampolineMaze, CannotDoStepOnFinishedMaze
 
 
 def test_empty_maze_is_already_finished():
@@ -23,5 +25,19 @@ def test_maze_with_one_and_two_is_not_finished_after_one_step():
 
 def test_maze_with_one_and_two_is_finished_after_two_steps():
     maze = TrampolineMaze([1, 2])
+    maze.process_until_finished()
+    assert 2 == maze.get_steps_done()
+
+
+def test_do_step_on_finished_maze_raises_exception():
+    maze = TrampolineMaze([])
+    maze.process_until_finished()
+    with pytest.raises(CannotDoStepOnFinishedMaze):
+        maze.do_step()
+
+
+@pytest.mark.skip
+def test_maze_with_zero_is_finished_after_two_step():
+    maze = TrampolineMaze([0])
     maze.process_until_finished()
     assert 2 == maze.get_steps_done()
