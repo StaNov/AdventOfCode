3bbaf628ea40306204df89e217685083877d69d6
StaNov <stanovec@gmail.com>
2017-12-15 23:29:07 +0100

    maze to string

--- problems/days/day205/internal/trampolinemaze.py
+++ problems/days/day205/internal/trampolinemaze.py
@@ -26,3 +26,12 @@ class TrampolineMaze:
 
     def _value_to_add_to_source_item(self, jump_distance):
         return 1
+
+    def __str__(self) -> str:
+        def format_item_to_string(index_and_number):
+            index, number = index_and_number
+            format_string = "({})" if index == self._current_index else " {} "
+            return format_string.format(number)
+
+        strings = list(map(format_item_to_string, enumerate(self._values)))
+        return ", ".join(strings)
--- problems/days/day205/internal/trampolinemaze_test.py
+++ problems/days/day205/internal/trampolinemaze_test.py
@@ -40,3 +40,9 @@ def test_maze_with_zero_is_finished_after_two_step():
     maze = TrampolineMaze([0])
     maze.process_until_finished()
     assert 2 == maze.get_steps_done()
+
+
+def test_to_string():
+    maze = TrampolineMaze([1, 2, 3, 4, 5])
+    maze._current_index = 3
+    assert " 1 ,  2 ,  3 , (4),  5 " == str(maze)
