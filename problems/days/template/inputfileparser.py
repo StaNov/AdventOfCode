91eef085f99686e3299fa3a7a14be8f5620fc3a4
StaNov <stanovec@gmail.com>
2017-12-17 21:43:44 +0100

    input parser moved to day 6, doh! :D

--- problems/days/template/inputfileparser.py
+++ problems/days/template/inputfileparser.py
@@ -3,4 +3,5 @@ from problems.utils import InputFileParser as BaseParser
 
 class InputFileParser(BaseParser):
     def parse(self, input_string):
-        return [int(x) for x in input_string.split()]
+        # TODO
+        return input_string.splitlines()
--- problems/days/template/inputfileparser_test.py
+++ problems/days/template/inputfileparser_test.py
@@ -1,17 +1,6 @@
 from .inputfileparser import InputFileParser
 
 
-def test_parse_empty_string():
-    assert [] == InputFileParser().parse("")
-
-
-def test_parse_one_number():
-    assert [1] == InputFileParser().parse("1")
-
-
-def test_parse_one_different_number():
-    assert [2] == InputFileParser().parse("2")
-
-
-def test_parse_two_numbers():
-    assert [1, 2] == InputFileParser().parse("1\t2")
+def test_parse():
+    # TODO
+    assert ["abc", "def", "ghi"] == InputFileParser().parse("abc\ndef\nghi")
