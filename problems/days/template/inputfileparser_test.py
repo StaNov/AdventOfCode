5cdbba173a595a8b27094518b9574a79e4623f92
StaNov <stanovec@gmail.com>
2017-12-17 21:36:34 +0100

    one number

--- problems/days/template/inputfileparser_test.py
+++ problems/days/template/inputfileparser_test.py
@@ -3,3 +3,7 @@ from .inputfileparser import InputFileParser
 
 def test_parse_empty_string():
     assert [] == InputFileParser().parse("")
+
+
+def test_parse_one_number():
+    assert [1] == InputFileParser().parse("1")
