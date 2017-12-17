0b74f6f5ffec25700e362094f508cce982bfd85f
StaNov <stanovec@gmail.com>
2017-12-17 22:07:39 +0100

    two banks

--- problems/days/day206/internal/memorybanks.py
+++ problems/days/day206/internal/memorybanks.py
@@ -1,9 +1,9 @@
 class MemoryBanks:
     def __init__(self, banks):
-        pass
+        self._banks = banks
 
     def process_until_cycle_is_found(self):
         pass
 
     def get_steps(self):
-        return 0
+        return 0 if not self._banks else 2
--- problems/days/day206/internal/memorybanks_test.py
+++ problems/days/day206/internal/memorybanks_test.py
@@ -1,7 +1,13 @@
 from . import MemoryBanks
 
 
-def test_helper_method():
+def test_empty_banks():
     banks = MemoryBanks([])
     banks.process_until_cycle_is_found()
     assert 0 == banks.get_steps()
+
+
+def test_two_banks():
+    banks = MemoryBanks([0, 1])
+    banks.process_until_cycle_is_found()
+    assert 2 == banks.get_steps()
