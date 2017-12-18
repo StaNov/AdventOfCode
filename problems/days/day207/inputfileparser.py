2b75646878f0f36afb61ac734ddc845e99171110
StaNov <stanovec@gmail.com>
2017-12-18 15:53:13 +0100

    day 7 copied from template with input

---
+++ problems/days/day207/inputfileparser.py
@@ -0,0 +1,7 @@
+from problems.utils import InputFileParser as BaseParser
+
+
+class InputFileParser(BaseParser):
+    def parse(self, input_string):
+        # TODO
+        return input_string.splitlines()
