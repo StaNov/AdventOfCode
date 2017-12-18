6909a94d2953df5345d22cdc421190abd97acfd1
StaNov <stanovec@gmail.com>
2017-12-18 17:19:01 +0100

    creating program tree, three programs

--- problems/days/day207/internal/programtree.py
+++ problems/days/day207/internal/programtree.py
@@ -2,16 +2,26 @@ class ProgramTree:
     def __init__(self, input_):
         self._root = None
 
-        for name, number, subprograms in input_:
-            if not self._root or self._root in subprograms:
-                self._root = name
+        programs = {}
 
-    def get_root_program(self):
-        return self._root
+        while input_:
+            processed_program = input_.pop(0)
+            name, number, subprograms = processed_program
+
+            if set(subprograms) <= set(programs.keys()):
+                new_program = Program(name, number, [programs[subprogram_name] for subprogram_name in subprograms])
+                programs[name] = new_program
+                if not self._root or self._root.name in subprograms:
+                    self._root = new_program
+            else:
+                input_.append(processed_program)
+
+    def get_root_program_name(self):
+        return self._root.name
 
 
-# class Program:
-#     def __init__(self, name, number, subprograms):
-#         self.name = name
-#         self.number = number
-#         self.subprograms = subprograms
+class Program:
+    def __init__(self, name, number, subprograms):
+        self.name = name
+        self.number = number
+        self.subprograms = subprograms
