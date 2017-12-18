b1d24bbd62534994e58a4a6426c0e8bd3cc1de75
StaNov <stanovec@gmail.com>
2017-12-18 16:00:30 +0100

    one line

--- problems/days/day207/inputfileparser.py
+++ problems/days/day207/inputfileparser.py
@@ -1,7 +1,14 @@
+import re
+
 from problems.utils import InputFileParser as BaseParser
 
 
 class InputFileParser(BaseParser):
     def parse(self, input_string):
-        return {}
+        if not input_string:
+            return {}
+
+        match = re.fullmatch("(.+) \((\d+)\)", input_string)
+        name, number = match.groups()
+        return {name: int(number)}
         # return input_string.splitlines()
--- problems/days/day207/inputfileparser_test.py
+++ problems/days/day207/inputfileparser_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 from .inputfileparser import InputFileParser
 
 
@@ -7,6 +5,5 @@ def test_parse_empty():
     assert {} == InputFileParser().parse("")
 
 
-@pytest.mark.skip
 def test_parse_one_line():
     assert {"abc": 123} == InputFileParser().parse("abc (123)")
