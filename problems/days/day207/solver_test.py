30915314fc0b9593da6fbe72ce8356cadce28bb3
StaNov <stanovec@gmail.com>
2017-12-18 16:25:40 +0100

    part 1 full example

--- problems/days/day207/solver_test.py
+++ problems/days/day207/solver_test.py
@@ -8,10 +8,24 @@ def solver():
     return Solver()
 
 
+@pytest.mark.skip
 def test_1_full_example(solver):
-    # TODO
-    # assert 0 == solver.solve_1("test test")
-    pass
+    input_ = {
+        "pbga": (66, []),
+        "xhth": (57, []),
+        "ebii": (61, []),
+        "havc": (66, []),
+        "ktlj": (57, []),
+        "fwft": (72, ["ktlj", "cntj", "xhth"]),
+        "qoyq": (66, []),
+        "padx": (45, ["pbga", "havc", "qoyq"]),
+        "tknk": (41, ["ugml", "padx", "fwft"]),
+        "jptl": (61, []),
+        "ugml": (68, ["gyxo", "ebii", "jptl"]),
+        "gyxo": (61, []),
+        "cntj": (57, []),
+    }
+    assert "tknk" == solver.solve_1(input_)
 
 
 def test_2_full_example(solver):
