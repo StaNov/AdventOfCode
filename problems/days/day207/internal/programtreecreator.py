fe6afb71b7cfcd62bf891ed3caf1b08fe2994010
StaNov <stanovec@gmail.com>
2017-12-20 21:13:02 +0100

    more refactorings

--- problems/days/day207/internal/programtreecreator.py
+++ problems/days/day207/internal/programtreecreator.py
@@ -5,23 +5,33 @@ class ProgramTreeCreator:
     def __init__(self, input_):
         self._root = None
         self._input_to_process = input_
+        self._created_programs = {}
+
         self._create_program_tree()
+
+    def _create_program_tree(self):
+        while self._input_to_process:
+            current_input = self._input_to_process.pop(0)
+            current_subprograms = current_input[2]
+
+            if self._all_subprograms_were_already_created(current_subprograms):
+                self._create_new_program_and_update_root_if_needed(current_input)
+            else:
+                self._input_to_process.append(current_input)
+
+    def _all_subprograms_were_already_created(self, subprograms_names):
+        return set(subprograms_names) <= set(self._created_programs.keys())
+
+    def _create_new_program_and_update_root_if_needed(self, input_):
+        current_name, current_weight, current_subprograms = input_
+        subprograms = [self._created_programs[subprogram_name] for subprogram_name in current_subprograms]
+
+        new_program = Program(current_name, current_weight, subprograms)
+
+        self._created_programs[new_program.name] = new_program
+
+        if not self._root or self._root.name in current_subprograms:
+            self._root = new_program
 
     def get_root_program_name(self):
         return self._root.name
-
-    def _create_program_tree(self):
-        created_programs = {}
-
-        while self._input_to_process:
-            current_input = self._input_to_process.pop(0)
-            current_name, current_weight, current_subprograms = current_input
-
-            if set(current_subprograms) <= set(created_programs.keys()):
-                new_program = Program(current_name, current_weight, [created_programs[subprogram_name] for subprogram_name in current_subprograms])
-                created_programs[current_name] = new_program
-
-                if not self._root or self._root.name in current_subprograms:
-                    self._root = new_program
-            else:
-                self._input_to_process.append(current_input)
