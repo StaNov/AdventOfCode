c17ae17e8ab4d9ae18d3d198a2f91ac906dc84a6
StaNov <stanovec@gmail.com>
2017-12-21 14:53:25 +0100

    basic balanced variant

--- problems/days/day207/internal/balancechecker.py
+++ problems/days/day207/internal/balancechecker.py
@@ -1,2 +1,10 @@
+from . import weightcomputer
+
+
 def is_balanced(program):
-    return True
+    subprogram_weights = [weightcomputer.weight_of(subprogram) for subprogram in program.subprograms]
+    return _all_weights_are_the_same(subprogram_weights)
+
+
+def _all_weights_are_the_same(subprogram_weights):
+    return len(set(subprogram_weights)) <= 1
--- problems/days/day207/internal/balancechecker_test.py
+++ problems/days/day207/internal/balancechecker_test.py
@@ -1,6 +1,35 @@
+import pytest
+
 from . import balancechecker
 from .program import Program
 
 
 def test_is_balanced_single_program():
     assert balancechecker.is_balanced(Program("test", 1, []))
+
+
+def test_is_balanced_two_programs_unbalanced():
+    program = Program("root", 1, [
+        Program("child_1", 2, []),
+        Program("child_2", 3, []),
+    ])
+
+    assert not balancechecker.is_balanced(program)
+
+
+@pytest.mark.skip
+def test_is_balanced_nested_subprograms():
+    program = Program("root", 1, [
+        Program("child_1", 6, [
+            Program("child_1_1", 3, []),
+            Program("child_1_2", 3, []),
+            Program("child_1_3", 3, []),
+        ]),
+        Program("child_2", 3, [
+            Program("child_1_1", 4, []),
+            Program("child_1_2", 4, []),
+            Program("child_1_3", 4, []),
+        ]),
+    ])
+
+    assert balancechecker.is_balanced(program)
