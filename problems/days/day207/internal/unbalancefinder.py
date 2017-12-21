e543ac51417c15e434d3b7cdafd4cde570ae3d84
StaNov <stanovec@gmail.com>
2017-12-21 15:26:33 +0100

    find different index stub

--- problems/days/day207/internal/unbalancefinder.py
+++ problems/days/day207/internal/unbalancefinder.py
@@ -1,7 +1,16 @@
+from . import weightcomputer
+
+
 class UnbalanceFinder:
 
     def __init__(self, root_program):
         self._root = root_program
 
     def find_unbalanced_program(self):
-        return self._root.subprograms[2]
+        subprogram_weights = [weightcomputer.weight_of(subprogram) for subprogram in self._root.subprograms]
+        different_index = _find_different_index(subprogram_weights)
+        return self._root.subprograms[different_index]
+
+
+def _find_different_index(list):
+    return 1
--- problems/days/day207/internal/unbalancefinder_test.py
+++ problems/days/day207/internal/unbalancefinder_test.py
@@ -1,15 +1,29 @@
 import pytest
 
+from . import unbalancefinder
 from .unbalancefinder import UnbalanceFinder
 from .program import Program
 
 
+@pytest.mark.skip
 def test_three_programs_one_unbalanced():
     unbalanced_program = Program("unbalanced", 8, [])
     unbalanced_tree = Program("root", 1, [
         Program("balanced_1", 5, []),
         Program("balanced_2", 5, []),
         unbalanced_program,
+    ])
+
+    assert unbalanced_program is UnbalanceFinder(unbalanced_tree).find_unbalanced_program()
+
+
+@pytest.mark.skip
+def test_three_programs_one_unbalanced_another():
+    unbalanced_program = Program("unbalanced", 4, [])
+    unbalanced_tree = Program("root", 1, [
+        Program("balanced_1", 2, []),
+        unbalanced_program,
+        Program("balanced_2", 2, []),
     ])
 
     assert unbalanced_program is UnbalanceFinder(unbalanced_tree).find_unbalanced_program()
@@ -27,3 +41,7 @@ def test_three_programs_under_one_middle():
     ])
 
     assert unbalanced_program is UnbalanceFinder(unbalanced_tree).find_unbalanced_program()
+
+
+def test_different_index():
+    assert 1 == unbalancefinder._find_different_index([1, 2, 1, 1, 1, 1])
