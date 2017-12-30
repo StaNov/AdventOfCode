bbd119de20ec1a8d2737a83632ee47bc32d43a37
StaNov <stanovec@gmail.com>
2017-12-30 18:55:07 +0100

    instruction type stub

--- adventofcode/problems/days/day208/inputfileparser_test.py
+++ adventofcode/problems/days/day208/inputfileparser_test.py
@@ -1,4 +1,5 @@
 from .inputfileparser import InputFileParser
+from .parsedinput import Instruction
 
 
 def test_register_name_1():
@@ -9,3 +10,8 @@ def test_register_name_1():
 def test_register_name_2():
     parsed_input = InputFileParser().parse("cde inc 5 if a > 1")
     assert "cde" == parsed_input.instructions[0].register_name
+
+
+def test_instruction_type():
+    parsed_input = InputFileParser().parse("b inc 5 if a > 1")
+    assert Instruction.Type.INC == parsed_input.instructions[0].type
--- adventofcode/problems/days/day208/parsedinput.py
+++ adventofcode/problems/days/day208/parsedinput.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 class ParsedInput:
     def __init__(self, register_name):
         self.instructions = [Instruction(register_name)]
@@ -6,3 +9,7 @@ class ParsedInput:
 class Instruction:
     def __init__(self, register_name):
         self.register_name = register_name
+        self.type = Instruction.Type.INC
+
+    class Type(Enum):
+        INC = 0
