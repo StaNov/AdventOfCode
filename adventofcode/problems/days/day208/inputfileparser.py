7b90997f1664aafcc6734b5e2a13cfbc78899409
StaNov <stanovec@gmail.com>
2017-12-30 20:04:09 +0100

    string to instruction type moved to parser

--- adventofcode/problems/days/day208/inputfileparser.py
+++ adventofcode/problems/days/day208/inputfileparser.py
@@ -1,8 +1,19 @@
 from adventofcode.problems.utils import InputFileParser as BaseParser
-from .parsedinput import ParsedInput
+from .parsedinput import ParsedInput, Instruction
 
 
 class InputFileParser(BaseParser):
     def parse(self, input_string):
         register_name, instruction_type, __, __, __, __, __ = input_string.split()
-        return ParsedInput(register_name, instruction_type)
+        return ParsedInput(
+            register_name,
+            string_to_instruction_type(instruction_type)
+        )
+
+
+def string_to_instruction_type(string):
+    string_to_type = {
+        "inc": Instruction.Type.INC,
+        "dec": Instruction.Type.DEC
+    }
+    return string_to_type[string]
--- adventofcode/problems/days/day208/parsedinput.py
+++ adventofcode/problems/days/day208/parsedinput.py
@@ -9,16 +9,8 @@ class ParsedInput:
 class Instruction:
     def __init__(self, register_name, instruction_type):
         self.register_name = register_name
-        self.type = Instruction.Type.parse(instruction_type)
+        self.type = instruction_type
 
     class Type(Enum):
         INC = 0
         DEC = 1
-
-        @staticmethod
-        def parse(string):
-            string_to_type = {
-                "inc": Instruction.Type.INC,
-                "dec": Instruction.Type.DEC
-            }
-            return string_to_type[string]
