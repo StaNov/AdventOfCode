c16cfc675c17495b680c0d3185c2197c5411a610
StaNov <stanovec@gmail.com>
2017-12-31 15:07:24 +0100

    apply instruction

--- adventofcode/problems/days/day208/internal/registercalculator.py
+++ adventofcode/problems/days/day208/internal/registercalculator.py
@@ -1,7 +1,10 @@
 class RegisterCalculator:
     def __init__(self):
-        self.result = 0
+        self.instruction_applied = False
+
+    def apply_instruction(self, instruction):
+        self.instruction_applied = True
 
     @property
     def highest_value(self):
-        return 0
+        return 10 if self.instruction_applied else 0
--- adventofcode/problems/days/day208/internal/registercalculator_test.py
+++ adventofcode/problems/days/day208/internal/registercalculator_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from . import RegisterCalculator
+from ..parsedinput import Instruction
 
 
 @pytest.fixture
@@ -10,3 +11,8 @@ def calculator():
 
 def test_zero_is_highest_value_after_calculator_is_created(calculator):
     assert 0 == calculator.highest_value
+
+
+def test_apply_one_instruction(calculator):
+    calculator.apply_instruction(Instruction("a", Instruction.Type.INC, 10, "a", Instruction.ConditionType.EQUALS, 0))
+    assert 10 == calculator.highest_value
