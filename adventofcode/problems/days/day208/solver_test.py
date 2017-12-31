e014372754802d639de53b601c1b863b5f0fcad3
StaNov <stanovec@gmail.com>
2017-12-31 14:29:19 +0100

    full example added

--- adventofcode/problems/days/day208/solver_test.py
+++ adventofcode/problems/days/day208/solver_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+from .parsedinput import ParsedInput, Instruction
 from .solver import Solver
 
 
@@ -8,11 +9,23 @@ def solver():
     return Solver()
 
 
-def test_1_full_example(solver):
+@pytest.fixture
+def test_input():
+    parsed_input = ParsedInput()
+
+    parsed_input.add_instruction("b", Instruction.Type.INC, 5, "a", Instruction.ConditionType.GREATER, 1)
+    parsed_input.add_instruction("a", Instruction.Type.INC, 1, "b", Instruction.ConditionType.LESSER, 5)
+    parsed_input.add_instruction("c", Instruction.Type.DEC, -10, "a", Instruction.ConditionType.GREATER_EQUALS, 1)
+    parsed_input.add_instruction("c", Instruction.Type.INC, -20, "c", Instruction.ConditionType.EQUALS, 10)
+
+    return parsed_input
+
+
+def test_1_full_example(solver, test_input):
     # TODO
-    assert 0 == solver.solve_1("test test")
+    assert 0 == solver.solve_1(test_input)
 
 
-def test_2_full_example(solver):
+def test_2_full_example(solver, test_input):
     # TODO
     assert 0 == solver.solve_2("test test")
