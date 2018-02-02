34e32b9983fbb834dc6bbb20479e0c69e20976b3
StaNov <stanovec@gmail.com>
2018-02-02 22:58:57 +0100

    registers made private

--- adventofcode/problems/days/day208/internal/registercalculator.py
+++ adventofcode/problems/days/day208/internal/registercalculator.py
@@ -3,7 +3,7 @@ from ..parsedinput import Instruction
 
 class RegisterCalculator:
     def __init__(self):
-        self.registers = {}
+        self._registers = {}
 
     def apply_instruction(self, instruction: Instruction):
         self.initialize_required_registers(instruction)
@@ -13,21 +13,21 @@ class RegisterCalculator:
 
         # TODO refactor - call the instruction or some instruction applier
         if instruction.type == Instruction.Type.INC:
-            self.registers[instruction.register_name] += instruction.value_to_apply
+            self._registers[instruction.register_name] += instruction.value_to_apply
         elif instruction.type == Instruction.Type.DEC:
-            self.registers[instruction.register_name] -= instruction.value_to_apply
+            self._registers[instruction.register_name] -= instruction.value_to_apply
 
     def initialize_required_registers(self, instruction):
         registers_to_initialize = instruction.register_name, instruction.condition_register
 
         for register_name in registers_to_initialize:
-            if register_name not in self.registers.keys():
-                self.registers[register_name] = 0
+            if register_name not in self._registers.keys():
+                self._registers[register_name] = 0
 
     def condition_is_met(self, instruction):
-        return (self.registers[instruction.condition_register] < instruction.condition_value
+        return (self._registers[instruction.condition_register] < instruction.condition_value
                 or instruction.condition_type != Instruction.ConditionType.LESSER)
 
     @property
     def highest_value(self):
-        return max(self.registers.values(), default=0)
+        return max(self._registers.values(), default=0)
