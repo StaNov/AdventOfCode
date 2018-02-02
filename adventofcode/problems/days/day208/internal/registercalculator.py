2bf3ddb46efb0672ffc5b2ed744b2a7d85ed31bd
StaNov <stanovec@gmail.com>
2018-02-02 23:06:54 +0100

    register calculator now uses registers

--- adventofcode/problems/days/day208/internal/registercalculator.py
+++ adventofcode/problems/days/day208/internal/registercalculator.py
@@ -1,33 +1,27 @@
+from .registers import Registers
 from ..parsedinput import Instruction
 
 
 class RegisterCalculator:
     def __init__(self):
-        self._registers = {}
+        self._registers = Registers()
 
     def apply_instruction(self, instruction: Instruction):
-        self.initialize_required_registers(instruction)
-
         if not self.condition_is_met(instruction):
             return
 
         # TODO refactor - call the instruction or some instruction applier
-        if instruction.type == Instruction.Type.INC:
-            self._registers[instruction.register_name] += instruction.value_to_apply
-        elif instruction.type == Instruction.Type.DEC:
-            self._registers[instruction.register_name] -= instruction.value_to_apply
-
-    def initialize_required_registers(self, instruction):
-        registers_to_initialize = instruction.register_name, instruction.condition_register
-
-        for register_name in registers_to_initialize:
-            if register_name not in self._registers.keys():
-                self._registers[register_name] = 0
+        value_to_apply = instruction.value_to_apply
+        if instruction.type == Instruction.Type.DEC:
+            value_to_apply = - value_to_apply
+            
+        self._registers.add(instruction.register_name, value_to_apply)
 
     def condition_is_met(self, instruction):
-        return (self._registers[instruction.condition_register] < instruction.condition_value
+        return (self._registers.get(instruction.condition_register) < instruction.condition_value
                 or instruction.condition_type != Instruction.ConditionType.LESSER)
 
     @property
     def highest_value(self):
-        return max(self._registers.values(), default=0)
+        values = [value for _, value in self._registers.registers]
+        return max(values, default=0)
