2161dbf1d88080a56d52d56f3d992119d12c18ca
StaNov <stanovec@gmail.com>
2018-02-02 20:38:41 +0100

    test for failing condition

--- adventofcode/problems/days/day208/internal/registercalculator.py
+++ adventofcode/problems/days/day208/internal/registercalculator.py
@@ -9,6 +9,15 @@ class RegisterCalculator:
         if instruction.register_name not in self.registers.keys():
             self.registers[instruction.register_name] = 0
 
+        if instruction.condition_register not in self.registers.keys():
+            self.registers[instruction.condition_register] = 0
+
+        condition_register_value = self.registers[instruction.condition_register]
+
+        if condition_register_value >= instruction.condition_value and instruction.condition_type == Instruction.ConditionType.LESSER:
+            return
+
+        # TODO refactor - call the instruction or some instruction applier
         if instruction.type == Instruction.Type.INC:
             self.registers[instruction.register_name] += instruction.value_to_apply
         elif instruction.type == Instruction.Type.DEC:
--- adventofcode/problems/days/day208/internal/registercalculator_test.py
+++ adventofcode/problems/days/day208/internal/registercalculator_test.py
@@ -49,6 +49,13 @@ def test_apply_two_instructions_same_register(calculator):
 
 def test_decrement_instruction(calculator):
     calculator.apply_instruction(
-        Instruction("a", Instruction.Type.DEC, 10, "a", Instruction.ConditionType.GREATER_EQUALS, 0)
+        Instruction("a", Instruction.Type.DEC, 10, "a", Instruction.ConditionType.NOT_EQUALS, 0)
     )
     assert -10 == calculator.highest_value
+
+
+def test_false_condition_keeps_zero_value(calculator):
+    calculator.apply_instruction(
+        Instruction("a", Instruction.Type.INC, 20, "a", Instruction.ConditionType.LESSER, 0)
+    )
+    assert 0 == calculator.highest_value
