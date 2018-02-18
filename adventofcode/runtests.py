392a0352a222c24f14c66c0d2a5c0c1a4cfea90b
StaNov <stanovec@gmail.com>
2018-02-18 12:51:00 +0100

    runtests redone to better readability

--- adventofcode/runtests.py
+++ adventofcode/runtests.py
@@ -1,4 +1,5 @@
 import sys
+from argparse import ArgumentParser
 
 import pytest
 
@@ -8,6 +9,39 @@ def run_tests(params):
     sys.exit(exit_code)
 
 
+def _script_called_without_arguments():
+    return len(sys.argv) == 1
+
+
 if __name__ == "__main__":
-    params = ["-m", "not time_expensive"] if len(sys.argv) == 1 else sys.argv[1:]
-    run_tests(params)
+
+    parser = ArgumentParser()
+    parser.add_argument('--all',
+                        action="store_true",
+                        help='Runs all tests.')
+    parser.add_argument('--onlycheap',
+                        action="store_true",
+                        help="Runs tests without those marked as time expensive. "
+                             "This is the default option when runtests is called without arguments.")
+    parser.add_argument('--onlyexpensive',
+                        action="store_true",
+                        help="Runs only tests marked as time expensive.")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) > 2:
+        print("Maximum arguments passed is one!")
+        parser.print_help()
+        sys.exit(1)
+
+    if args.all:
+        print("Running all tests...")
+        run_tests([])
+
+    if args.onlyexpensive:
+        print("Running only expensive tests...")
+        run_tests(["-m", "time_expensive"])
+
+    if args.onlycheap or _script_called_without_arguments():
+        print("Running only cheap tests...")
+        run_tests(["-m", "not time_expensive"])
