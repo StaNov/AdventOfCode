079461b917278c8bc719a16aa811e7bd7caeaa4e
StaNov <stanovec@gmail.com>
2018-02-25 21:09:51 +0100

    tests renamed to proper "_test" suffix so they run

--- adventofcode/problems/days/day208/internal/instruction/applier/applier_test.py
+++ adventofcode/problems/days/day208/internal/instruction/applier/applier_test.py
@@ -1,9 +1,10 @@
 from .applier import Applier
+from ..instructiontype import InstructionType
 from ..registersspy import RegistersSpy
 
 
 def test_value_is_applied_to_registers():
-    applier = Applier("a", 10)
+    applier = Applier("a", InstructionType.INC, 10)
     registers = RegistersSpy()
 
     applier.apply_on_registers(registers)
