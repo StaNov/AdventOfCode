a00efa6786ea3ac138f500e09e4bff3a5445cefd
StaNov <stanovec@gmail.com>
2018-02-25 21:04:12 +0100

    applier connected to rest

--- adventofcode/problems/days/day208/internal/instruction/applier/__init__.py
+++ adventofcode/problems/days/day208/internal/instruction/applier/__init__.py
@@ -1,10 +1,17 @@
 from . import incremental, decremental
+from .applier import Applier
 from ..instructiontype import InstructionType
 
+_value_to_apply_processors = {
+    InstructionType.INC: incremental.apply,
+    InstructionType.DEC: decremental.apply,
+}
 
-def get_applier_for_instruction_type(instruction_type):
-    appliers = {
-        InstructionType.INC: incremental.apply,
-        InstructionType.DEC: decremental.apply,
-    }
-    return appliers[instruction_type]
+
+def get_applier_for_instruction_type(register_name, instruction_type, value_to_apply):
+    value_to_apply_processor = _value_to_apply_processors[instruction_type]
+
+    return Applier(
+        register_name,
+        value_to_apply_processor(value_to_apply)
+    )
--- adventofcode/problems/days/day208/internal/instruction/instruction.py
+++ adventofcode/problems/days/day208/internal/instruction/instruction.py
@@ -6,12 +6,12 @@ class Instruction:
                  register_name,
                  instruction_type,
                  value_to_apply):
-        self.register_name = register_name
+        self._applier = applier.get_applier_for_instruction_type(register_name, instruction_type, value_to_apply)
+
         # TODO only for testing, can be removed somehow?
+        self.register_name = register_name
         self._instruction_type = instruction_type
-        self._applier = applier.get_applier_for_instruction_type(instruction_type)
         self._value_to_apply = value_to_apply
 
     def apply_on_registers(self, registers):
-        value_to_apply = self._applier(self._value_to_apply)
-        registers.add(self.register_name, value_to_apply)
+        self._applier.apply_on_registers(registers)
