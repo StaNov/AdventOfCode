bb9b2cb551677dca33386de1ad8d8c95547949d2
StaNov <stanovec@gmail.com>
2018-05-22 21:37:23 +0200

    refactored parsedinput

--- adventofcode/problems/days/day208/internal/parsedinput.py
+++ adventofcode/problems/days/day208/internal/parsedinput.py
@@ -3,7 +3,7 @@ from .instruction import InstructionWithCondition
 
 class ParsedInput:
     def __init__(self):
-        self.instructions = []
+        self._instructions = []
 
     def add_instruction(self,
                         register_name,
@@ -12,10 +12,14 @@ class ParsedInput:
                         condition_register,
                         condition_type,
                         condition_value):
-        self.instructions.append(InstructionWithCondition(
+        self._instructions.append(InstructionWithCondition(
             register_name,
             instruction_type,
             value_to_apply,
             condition_register,
             condition_type,
             condition_value))
+
+    @property
+    def instructions(self):
+        return tuple(self._instructions)
