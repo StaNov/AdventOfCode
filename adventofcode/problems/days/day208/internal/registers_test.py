8d2df3cb4fc5773e8d5160b043415bf1880af25d
StaNov <stanovec@gmail.com>
2018-06-03 21:56:54 +0200

    tests renamed

--- adventofcode/problems/days/day208/internal/registers_test.py
+++ adventofcode/problems/days/day208/internal/registers_test.py
@@ -8,17 +8,17 @@ def registers():
     return Registers()
 
 
-def test_get_untouched_register(registers: Registers):
+def test_any_register_has_default_value_after_creation(registers: Registers):
     assert 0 == registers.get("untouched register")
 
 
-def test_get_setted_register(registers: Registers):
+def test_get_touched_register(registers: Registers):
     registers.add("register", 12)
     assert 12 == registers.get("register")
 
 
-def test_get_other_register(registers: Registers):
-    registers.add("setted register", 10)
+def test_untouched_register_has_zero_value(registers: Registers):
+    registers.add("touched register", 10)
     assert 0 == registers.get("untouched register")
 
 
