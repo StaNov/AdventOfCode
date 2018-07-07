85e6d0976847ab193d06883d1abd0bfb842226d7
StaNov <stanovec@gmail.com>
2018-07-07 22:57:38 +0200

    input text parser redone to fixture

--- adventofcode/problems/days/day208/internal/inputtextparser/inputtextparser_test.py
+++ adventofcode/problems/days/day208/internal/inputtextparser/inputtextparser_test.py
@@ -1,67 +1,74 @@
-from adventofcode.problems.days.day208.internal.inputtextparser import InputTextParser
-from adventofcode.problems.days.day208.internal.instruction import InstructionType, InstructionConditionType
+import pytest
+
+from . import InputTextParser
+from ...internal import InstructionType, InstructionConditionType
 
 
-def test_register_name():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond > 1")
+@pytest.fixture
+def input_text_parser():
+    return InputTextParser()
+
+
+def test_register_name(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond > 1")
     assert "reg" == parsed_input.instructions[0]._register_name
 
 
-def test_instruction_type_inc():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond > 1")
+def test_instruction_type_inc(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond > 1")
     assert InstructionType.INC == parsed_input.instructions[0]._instruction_type
 
 
-def test_instruction_type_dec():
-    parsed_input = InputTextParser().parse("reg dec 5 if cond > 1")
+def test_instruction_type_dec(input_text_parser):
+    parsed_input = input_text_parser.parse("reg dec 5 if cond > 1")
     assert InstructionType.DEC == parsed_input.instructions[0]._instruction_type
 
 
-def test_value_to_apply():
-    parsed_input = InputTextParser().parse("reg inc 10001 if cond > 1")
+def test_value_to_apply(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 10001 if cond > 1")
     assert 10001 == parsed_input.instructions[0]._value_to_apply
 
 
-def test_condition_register():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond > 1")
+def test_condition_register(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond > 1")
     assert "cond" == parsed_input.instructions[0].condition_register
 
 
-def test_condition_type_greater():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond > 1")
+def test_condition_type_greater(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond > 1")
     assert InstructionConditionType.GREATER == parsed_input.instructions[0].condition_type
 
 
-def test_condition_type_greater_equals():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond >= 1")
+def test_condition_type_greater_equals(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond >= 1")
     assert InstructionConditionType.GREATER_EQUALS == parsed_input.instructions[0].condition_type
 
 
-def test_condition_type_lesser():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond < 1")
+def test_condition_type_lesser(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond < 1")
     assert InstructionConditionType.LESSER == parsed_input.instructions[0].condition_type
 
 
-def test_condition_type_lesser_equals():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond <= 1")
+def test_condition_type_lesser_equals(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond <= 1")
     assert InstructionConditionType.LESSER_EQUALS == parsed_input.instructions[0].condition_type
 
 
-def test_condition_type_equals():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond == 1")
+def test_condition_type_equals(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond == 1")
     assert InstructionConditionType.EQUALS == parsed_input.instructions[0].condition_type
 
 
-def test_condition_type_not_equals():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond != 1")
+def test_condition_type_not_equals(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond != 1")
     assert InstructionConditionType.NOT_EQUALS == parsed_input.instructions[0].condition_type
 
 
-def test_condition_value():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond != 98765")
+def test_condition_value(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond != 98765")
     assert 98765 == parsed_input.instructions[0].condition_value
 
 
-def test_multiple_lines():
-    parsed_input = InputTextParser().parse("reg inc 5 if cond > 1\n" * 3)
+def test_multiple_lines(input_text_parser):
+    parsed_input = input_text_parser.parse("reg inc 5 if cond > 1\n" * 3)
     assert 3 == len(parsed_input.instructions)
