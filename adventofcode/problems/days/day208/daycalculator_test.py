a54da84864e858f2ff82d3c2307eaaf9063187c6
StaNov <stanovec@gmail.com>
2018-07-07 22:10:52 +0200

    solver redone so it works for acceptance tests

--- adventofcode/problems/days/day208/daycalculator_test.py
+++ adventofcode/problems/days/day208/daycalculator_test.py
@@ -4,36 +4,32 @@ from .daycalculator import DayCalculator
 
 
 @pytest.fixture
-def main():
+def day_calculator():
     return DayCalculator()
 
 
-# TODO
-@pytest.mark.skip
-def test_main_1_on_empty_input(main):
-    assert 0 == main.calculate_part_1("")
+def test_main_1_on_empty_input():
+    assert 0 == DayCalculator("").calculate_part_1()
 
 
-# TODO
-@pytest.mark.skip
-def test_main_1_with_all_conditions_true(main):
+def test_main_1_with_all_conditions_true():
     custom_input = (
-        "a inc 10 if a = 0\n"
+        "a inc 10 if a == 0\n"
         "a inc 10 if b <= 0\n"
         "a inc 10 if c > -1\n"
         "a inc 10 if d < 2\n"
         "a dec -10 if e >= 0")
 
-    assert 50 == main.calculate_part_1(custom_input)
+    assert 50 == DayCalculator(custom_input).calculate_part_1()
 
 
-def test_main_1(main):
+def test_main_1(day_calculator):
     # TODO
-    # assert 0 == main.calculate_part_1()
+    # assert 0 == day_calculator.calculate_part_1()
     pass
 
 
-def test_main_2(main):
+def test_main_2(day_calculator):
     # TODO
-    # assert 0 == main.calculate_part_2()
+    # assert 0 == day_calculator.calculate_part_2()
     pass
--- adventofcode/problems/days/day208/solver.py
+++ adventofcode/problems/days/day208/solver.py
@@ -5,12 +5,13 @@ from .internal import RegisterCalculator
 class Solver(AbstractSolver):
     def __init__(self):
         super().__init__()
-        self.helper = RegisterCalculator()
+        self.calculator = RegisterCalculator()
 
     def _solve_1_internal(self, input_):
-        # TODO
-        # return self.helper.helper_method(input_)
-        return 0
+        for instruction in input_.instructions:
+            self.calculator.apply_instruction(instruction)
+
+        return self.calculator.highest_value
 
     def _solve_2_internal(self, input_):
         # TODO
