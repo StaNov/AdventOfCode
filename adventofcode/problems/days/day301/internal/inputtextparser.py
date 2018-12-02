62245f92a07a2c50376d1932552663b3b64fbd93
StaNov <stanovec@gmail.com>
2018-12-02 17:20:55 +0100

    starting work on parser

--- adventofcode/problems/days/day301/internal/inputtextparser.py
+++ adventofcode/problems/days/day301/internal/inputtextparser.py
@@ -3,5 +3,4 @@ from adventofcode.problems.framework import DefaultInputTextParser
 
 class InputTextParser(DefaultInputTextParser):
     def parse(self, input_string):
-        # TODO
         return input_string.splitlines()
