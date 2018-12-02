530284de1e23812f87809a0359fc04d7b8c9c5d5
StaNov <stanovec@gmail.com>
2018-12-02 16:37:47 +0100

    first acceptance test

--- adventofcode/problems/days/day301/daycalculator_test.py
+++ adventofcode/problems/days/day301/daycalculator_test.py
@@ -8,6 +8,10 @@ def calculator():
     return DayCalculator()
 
 
+def test_empty_input_gives_zero_result():
+    assert 0 == DayCalculator("").calculate_part_1()
+
+
 def test_main_1(calculator):
     # TODO
     assert 0 == calculator.calculate_part_1()
