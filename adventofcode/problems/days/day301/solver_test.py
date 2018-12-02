771b6f55eaf9d6560693df8e247ceaefe59f6e47
StaNov <stanovec@gmail.com>
2018-12-02 16:49:26 +0100

    first solver test with empty input

--- adventofcode/problems/days/day301/solver_test.py
+++ adventofcode/problems/days/day301/solver_test.py
@@ -8,9 +8,8 @@ def solver():
     return Solver()
 
 
-def test_1_full_example(solver):
-    # TODO
-    assert 0 == solver.solve_1("test test")
+def test_1_empty_input_is_zero_result(solver):
+    assert 0 == solver.solve_1([])
 
 
 def test_2_full_example(solver):
