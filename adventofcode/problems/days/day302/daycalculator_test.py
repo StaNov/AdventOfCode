b1279812dbe13b69ed9464ebff7e45ece2f8ce18
StaNov <stanovec@gmail.com>
2018-12-04 21:00:28 +0100

    dummy example tests in day calculator 302

--- adventofcode/problems/days/day302/daycalculator_test.py
+++ adventofcode/problems/days/day302/daycalculator_test.py
@@ -8,9 +8,29 @@ def calculator():
     return DayCalculator()
 
 
+def test_example_1_1(calculator):
+    # TODO
+    assert 0 == DayCalculator("Part 1, example 1").calculate_part_1()
+
+
+def test_example_1_2(calculator):
+    # TODO
+    assert 0 == DayCalculator("Part 2, example 2").calculate_part_1()
+
+
 def test_main_1(calculator):
     # TODO
     assert 0 == calculator.calculate_part_1()
+
+
+def test_example_2_1(calculator):
+    # TODO
+    assert 0 == DayCalculator("Part 2, example 1").calculate_part_2()
+
+
+def test_example_2_2(calculator):
+    # TODO
+    assert 0 == DayCalculator("Part 2, example 2").calculate_part_2()
 
 
 def test_main_2(calculator):
