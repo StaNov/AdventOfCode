bf3f82e57966fd347bf2baa894b0da777cd8bc10
StaNov <stanovec@gmail.com>
2018-12-04 21:18:50 +0100

    skipping solver tests

--- adventofcode/problems/days/day302/solver_test.py
+++ adventofcode/problems/days/day302/solver_test.py
@@ -8,11 +8,15 @@ def solver():
     return Solver()
 
 
+# TODO
+@pytest.mark.skip
 def test_1_full_example(solver):
     # TODO
     assert 0 == solver.solve_1("test test")
 
 
+# TODO
+@pytest.mark.skip
 def test_2_full_example(solver):
     # TODO
     assert 0 == solver.solve_2("test test")
