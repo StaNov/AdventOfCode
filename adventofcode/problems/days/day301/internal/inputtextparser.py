062244496b2ff6cda5911e24a6cbcf99a0ccadd1
StaNov <stanovec@gmail.com>
2018-12-05 19:30:44 +0100

    simpler input parser

--- adventofcode/problems/days/day301/internal/inputtextparser.py
+++ adventofcode/problems/days/day301/internal/inputtextparser.py
@@ -4,14 +4,4 @@ from adventofcode.problems.framework import DefaultInputTextParser
 class InputTextParser(DefaultInputTextParser):
     def parse(self, input_string):
         input_lines = input_string.splitlines()
-        return [parse_input_line(x) for x in input_lines]
-
-
-def parse_input_line(value):
-    sign = value[0]
-    number = int(value[1:])
-
-    if sign == "-":
-        number = -number
-
-    return number
+        return [int(x) for x in input_lines]
