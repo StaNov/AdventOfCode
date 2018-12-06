18f5a00336321f77d893790596e54cc8e10ca052
StaNov <stanovec@gmail.com>
2018-12-06 22:55:47 +0100

    more lines in parser

--- adventofcode/problems/days/day303/internal/inputtextparser_test.py
+++ adventofcode/problems/days/day303/internal/inputtextparser_test.py
@@ -16,3 +16,19 @@ def test_parse_one_line():
     assert 34 == result.position_y
     assert 56 == result.size_x
     assert 789 == result.size_y
+
+
+def test_parse_more_lines():
+    result_list = InputTextParser().parse(
+        "#123 @ 1,456: 98x765\n"
+        "#12 @ 12,45: 987x65\n"
+        "#1 @ 123,4: 9876x5\n"
+    )
+
+    assert 3 == len(result_list)
+
+    assert 123 == result_list[0].id
+    assert 12 == result_list[1].position_x
+    assert 4 == result_list[2].position_y
+    assert 987 == result_list[1].size_x
+    assert 5 == result_list[2].size_y
