7d8b889356b1f3abbe8210e3f731b3d4ff841ea2
StaNov <stanovec@gmail.com>
2018-12-09 22:03:05 +0100

    solver test no patches

--- adventofcode/problems/days/day303/solver_test.py
+++ adventofcode/problems/days/day303/solver_test.py
@@ -8,9 +8,8 @@ def solver():
     return Solver()
 
 
-def test_1_something(solver):
-    # TODO
-    assert 0 == solver.solve_1("test test")
+def test_1_no_patches(solver):
+    assert 0 == solver.solve_1([])
 
 
 def test_2_something(solver):
