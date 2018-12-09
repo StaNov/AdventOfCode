d8ae44236640b16aca588b11f7d11f09c441b10c
StaNov <stanovec@gmail.com>
2018-12-09 23:25:01 +0100

    apply patches refactored

--- adventofcode/problems/days/day303/solver.py
+++ adventofcode/problems/days/day303/solver.py
@@ -8,12 +8,14 @@ class Solver(AbstractSolver):
         self.santa_suit = SantaSuit()
 
     def _solve_1_internal(self, input_):
-        for patch in input_:
-            self.santa_suit.saw_patch(patch)
-
+        self.apply_patches(input_)
         return self.santa_suit.get_overlapping_count()
 
     def _solve_2_internal(self, input_):
         # TODO
         # return self.santa_suit.helper_method(input_)
         return 0
+
+    def apply_patches(self, input_):
+        for patch in input_:
+            self.santa_suit.saw_patch(patch)
