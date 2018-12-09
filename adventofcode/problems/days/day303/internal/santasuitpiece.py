2dd1837cebbc98bc623e9b5c5353857e439be8d4
StaNov <stanovec@gmail.com>
2018-12-09 22:27:31 +0100

    renamed id to patch_id

--- adventofcode/problems/days/day303/internal/santasuitpiece.py
+++ adventofcode/problems/days/day303/internal/santasuitpiece.py
@@ -1,7 +1,7 @@
 class SantaSuitPiece:
-    def __init__(self, id, position_x, position_y, size_x, size_y) -> None:
+    def __init__(self, patch_id, position_x, position_y, size_x, size_y) -> None:
         super().__init__()
-        self.id = int(id)
+        self.id = int(patch_id)
         self.position_x = int(position_x)
         self.position_y = int(position_y)
         self.size_x = int(size_x)
