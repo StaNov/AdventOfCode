1c00fbef9653c1ec968e519a4c58f525e90c8d76
StaNov <stanovec@gmail.com>
2018-12-09 22:01:34 +0100

    added new simple acceptance test

--- adventofcode/problems/days/day303/daycalculator_test.py
+++ adventofcode/problems/days/day303/daycalculator_test.py
@@ -14,6 +14,14 @@ def test_example_1_empty_input(calculator):
 
 # TODO
 @pytest.mark.skip
+def test_example_1_two_same_squares(calculator):
+    assert 4 == DayCalculator(
+        "#1 @ 1,1: 2x2\n"
+        "#2 @ 1,1: 2x2\n").calculate_part_1()
+
+
+# TODO
+@pytest.mark.skip
 def test_example_1_1(calculator):
     assert 4 == DayCalculator(
         "#1 @ 1,3: 4x4\n"
