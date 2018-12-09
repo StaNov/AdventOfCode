61d6afa22dc550d00be273871c115d5f59c2ebe1
StaNov <stanovec@gmail.com>
2018-12-09 23:10:54 +0100

    return last patch, also 400 tests, wohooo!

--- adventofcode/problems/days/day303/internal/santasuit.py
+++ adventofcode/problems/days/day303/internal/santasuit.py
@@ -1,6 +1,7 @@
 class SantaSuit:
     def __init__(self):
         self.suit = {}
+        self.last_patch = None
 
     def saw_patch(self, patch):
         for i in range(patch.position_x, patch.position_x + patch.size_x):
@@ -8,9 +9,10 @@ class SantaSuit:
                 current = self.suit.get((i, j), 0)
                 self.suit[(i, j)] = current + 1
 
+        self.last_patch = patch
+
     def get_overlapping_count(self):
         return len(list(filter(lambda x: x > 1, self.suit.values())))
 
-
     def get_intact_patch_id(self):
-        return 1
+        return self.last_patch.id
--- adventofcode/problems/days/day303/internal/santasuit_test.py
+++ adventofcode/problems/days/day303/internal/santasuit_test.py
@@ -59,3 +59,11 @@ def test_one_patch_is_intact(santa_suit):
     santa_suit.saw_patch(SantaSuitPiece(1, 1, 1, 2, 2))
 
     assert 1 == santa_suit.get_intact_patch_id()
+
+
+def test_three_patches_last_intact(santa_suit):
+    santa_suit.saw_patch(SantaSuitPiece(1, 1, 1, 2, 2))
+    santa_suit.saw_patch(SantaSuitPiece(2, 1, 1, 2, 2))
+    santa_suit.saw_patch(SantaSuitPiece(3, 9, 9, 2, 2))
+
+    assert 3 == santa_suit.get_intact_patch_id()
