6cc5f29a5838f029068428e528f716460b72bb5f
StaNov <stanovec@gmail.com>
2018-12-22 20:40:55 +0100

    renamed tests

--- adventofcode/problems/days/day313/internal/oremine_test.py
+++ adventofcode/problems/days/day313/internal/oremine_test.py
@@ -14,13 +14,13 @@ def test_create_ore_mine_with_one_car():
     assert (0, 0) == cars[0].position
 
 
-def test_create_car_moves_down_after_one_step():
+def test_car_moves_down_after_one_step():
     mine = OreMine([Car((0, 0))])
     mine.simulate_step()
     assert (0, 1) == mine.cars[0].position
 
 
-def test_create_car_moves_down_after_one_step_another_location():
+def test_car_moves_down_after_one_step_another_location():
     mine = OreMine([Car((5, 5))])
     mine.simulate_step()
     assert (5, 6) == mine.cars[0].position
