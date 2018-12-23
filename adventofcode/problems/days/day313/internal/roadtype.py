4198189a19f1065bd0866ad028618485365d1fc0
StaNov <stanovec@gmail.com>
2018-12-23 22:47:30 +0100

    left to up

--- adventofcode/problems/days/day313/internal/roadtype.py
+++ adventofcode/problems/days/day313/internal/roadtype.py
@@ -1,8 +1,14 @@
 from enum import Enum
 
+from .direction import Direction
+
 
 class RoadType(Enum):
     STRAIGHT = 0
+    LEFT_TO_UP = 1
 
     def rotate(self, initial_rotation):
-        return initial_rotation
+        if self is RoadType.STRAIGHT:
+            return initial_rotation
+
+        return Direction.UP
--- adventofcode/problems/days/day313/internal/roadtype_test.py
+++ adventofcode/problems/days/day313/internal/roadtype_test.py
@@ -1,9 +1,17 @@
-from adventofcode.problems.days.day313.internal.roadtype import RoadType
+from .direction import Direction
+from .roadtype import RoadType
 
 
-def test_straight_does_not_change_rotation():
+def test_straight_does_not_change_direction():
     initial_rotation = object()
 
     final_rotation = RoadType.STRAIGHT.rotate(initial_rotation)
 
     assert final_rotation is initial_rotation
+
+
+def test_left_to_up_turns_left_to_up():
+    initial_rotation = Direction.LEFT
+    final_rotation = RoadType.LEFT_TO_UP.rotate(initial_rotation)
+
+    assert final_rotation is Direction.UP
