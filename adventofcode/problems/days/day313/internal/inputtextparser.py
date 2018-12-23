8dd04bf47ed5e8511ca63c4571d884e69ed7594e
StaNov <stanovec@gmail.com>
2018-12-23 14:28:47 +0100

    one car facing up

--- adventofcode/problems/days/day313/internal/inputtextparser.py
+++ adventofcode/problems/days/day313/internal/inputtextparser.py
@@ -1,7 +1,14 @@
+from .direction import Direction
+from .car import Car
 from .oremine import OreMine
 from adventofcode.problems.framework import DefaultInputTextParser
 
 
 class InputTextParser(DefaultInputTextParser):
     def parse(self, input_string):
-        return OreMine([])
+        cars = []
+
+        if input_string:
+            cars.append(Car((0, 0), Direction.UP))
+
+        return OreMine(cars)
--- adventofcode/problems/days/day313/internal/inputtextparser_test.py
+++ adventofcode/problems/days/day313/internal/inputtextparser_test.py
@@ -4,3 +4,10 @@ from .inputtextparser import InputTextParser
 def test_empty_input():
     parsed = InputTextParser().parse("")
     assert len(parsed.cars) == 0
+
+
+def test_one_car_facing_up():
+    parsed = InputTextParser().parse("^")
+
+    assert len(parsed.cars) == 1
+    assert parsed.cars[0].position == (0, 0)
