6f45cc516e283af70df7d6d8d3f5044c6f7f7938
StaNov <stanovec@gmail.com>
2018-12-23 14:54:37 +0100

    solver wired to parsed input

--- adventofcode/problems/days/day313/solver.py
+++ adventofcode/problems/days/day313/solver.py
@@ -1,15 +1,12 @@
 from adventofcode.problems.framework import AbstractSolver
-from .internal import OreMine
 
 
 class Solver(AbstractSolver):
-    def __init__(self):
-        super().__init__()
-        self.helper = OreMine([])
 
-    def _solve_1_internal(self, input_):
-        # TODO
-        return "3,3"
+    def _solve_1_internal(self, ore_mine):
+        collision_position = ore_mine.simulate_until_first_collision()
+        position_x, position_y = collision_position
+        return "{},{}".format(position_x, position_y)
 
     def _solve_2_internal(self, input_):
         # TODO
